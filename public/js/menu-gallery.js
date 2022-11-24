c0e29386de8757f9937158281b9c3b6538eb68eb
NIMI3073 <oluwalonimimary@gmail.com>
2022-11-24 11:37:49 +0100

    js gallery changes

--- public/js/menu-gallery.js
+++ public/js/menu-gallery.js
@@ -1,11 +1,15 @@
 const addToCart = (menuId) => {
-    console.log(menuId)
-
-    axios.post('http://localhost:8000/api/carts',{menu_id : menuId}).then((response)=>{
-    console.log(response.data)
-    alert("Item Added to Cart Successfully")
-    }).catch((error)=>{
-        console.log(error.response)
-        alert("error Adding Item to Cart")
-    })
+  console.log(menuId)
+  let config = {
+      headers: {
+        Authorization: "Bearer "+ localStorage.getItem('token'),
+      }
+    }
+  axios.post('http://localhost:8000/api/carts',{menu_id : menuId}, config).then((response)=>{
+  console.log(response.data)
+  alert("Item Added to Cart Successfully")
+  }).catch((error)=>{
+      console.log(error.response)
+      alert("error Adding Item to Cart")
+  })
 }
